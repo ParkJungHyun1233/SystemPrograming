7b34404c3b9f4cd3cd2330b013e2fc5cc7608ba7
ParkJungHyun1233 <149235089+ParkJungHyun1233@users.noreply.github.com>
2023-11-20 17:27:11 +0900

    Add files via upload

---
+++ Lab3/num5/func.c
@@ -0,0 +1,16 @@
+#Include <stdlib.h>
+
+void exitfunc1(void);
+void exitfunc2(void);
+
+main(){
+	atexit(exitfunc1);
+	atexit(exitfunc2);
+	printf("This is main function.\n");
+}
+void exitfunc1(void){
+	printf("This is exit funtion 1.\n");
+}
+void exitfunc2(void){
+	printf("This is exit funtion 2.\n");
+}
